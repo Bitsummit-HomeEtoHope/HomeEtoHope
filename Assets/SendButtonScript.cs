0e89416f348a2c8d2d5276e45ae3a38d8e8cd8be
Kawasaki Yuichi <yuyux0910@gmail.com>
2023-05-21 23:42:19 +0900

    Design_Rotate,devからのプル

---
+++ Assets/SendButtonScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendButtonScript : MonoBehaviour
+{
+    // ボタンが押されたときに呼び出す関数
+    public void OnClick()
+    {
+        
+    }
+}
+
