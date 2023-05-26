d2de1f9c7451ed40fe553c54b9410a58bb027033
小左QAQ <lz1440918141@126.com>
2023-05-27 03:10:09 +0800

    camera_Change

--- Assets/Scripts/Camera/DualScreenDisplay.cs
+++ Assets/Scripts/Camera/DualScreenDisplay.cs
@@ -9,6 +9,16 @@ public class DualScreenDisplay : MonoBehaviour
 
     void Start()
     {
+        int width1 = Display.displays[1].systemWidth;
+        int height1 = Display.displays[1].systemHeight;
+        int width2 = Display.displays[2].systemWidth;
+        int height2 = Display.displays[2].systemHeight;
+        // 将Unity游戏窗口的分辨率设置为第一个外部屏幕的分辨率
+        Screen.SetResolution(width1, height1, true);
+        Screen.SetResolution(width2, height2, true);
+    
+    
+    
         // 获取所有屏幕的数量
         int displayCount = Display.displays.Length;
 
