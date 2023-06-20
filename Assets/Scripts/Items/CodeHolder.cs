e7b3606f1e08ea59b48ac242e0fa842994e38e22
mixyao <chunshuaimiao@yahoo.com>
2023-06-20 18:35:47 +0800

    Squashed commit of the following:
    commit 4654286db1a2006593c11d660963f1942b40e194
    Author: mixyao <chunshuaimiao@yahoo.com>
    Date:   Tue Jun 20 18:35:08 2023 +0800
    
        mixyao/06/20_log_00_hight_light
    
        changes(06/20):
        itemsmanager.cs(mate)
        codeholder.cs(mate)
        ... ... .prefab

--- Assets/Scripts/Items/CodeHolder.cs
+++ Assets/Scripts/Items/CodeHolder.cs
@@ -1,18 +1,34 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CodeHolder : MonoBehaviour
 {
-    public string code; // Stores the code of the object
-    public string tag; // Stores the tag of the object
+    public string code; // 存储物体的代码
+    public string tag; // 存储物体的标签
 
-    private ShowList_MRYM showList; // Reference to the ShowList_MRYM script
+    public Material defaultMaterial; // 默认材质
+    public Material highlightMaterial; // 高亮材质
+    private MeshRenderer meshRenderer; // 物体的 MeshRenderer 组件
+
+    public bool ihaveChild = false;
+
+    [System.Serializable]
+    public struct ChildRenderers
+    {
+        public MeshRenderer childRenderer; // 子物体的 MeshRenderer 组件
+        public Material childDefaultMaterial; // 子物体的默认材质
+        public Material childHighlightMaterial; // 子物体的高亮材质
+    }
+    public List<ChildRenderers> childRenderers = new List<ChildRenderers>();
+
+    private static CodeHolder selectedObject;
+
+    private ShowList_MRYM showList; // ShowList_MRYM 脚本的引用
 
     private void Start()
     {
-        // Add the object's name to the code variable when the object is instantiated and remove "(Clone)"
         code = gameObject.name.Replace("(Clone)", "");
-
-        // Get the object's tag
         tag = gameObject.tag;
         Debug.Log("Object Tag: " + tag);
 
@@ -24,6 +40,11 @@ public class CodeHolder : MonoBehaviour
                 showList.OpenList();
             }
         }
+
+        
+        
+            meshRenderer = GetComponent<MeshRenderer>();
+        
     }
 
     public string GetCode()
@@ -31,32 +52,66 @@ public class CodeHolder : MonoBehaviour
         return code;
     }
 
-    private bool isClicked = false; // State of whether the object has been clicked
+    private bool isClicked = false; // 记录物体是否被点击过
 
     private void OnMouseDown()
     {
+        if (selectedObject != null)
+        {
+            if (selectedObject.ihaveChild)
+            {
+                foreach (ChildRenderers childRenderer in selectedObject.childRenderers)
+                {
+                    childRenderer.childRenderer.material = childRenderer.childDefaultMaterial;
+                }
+            }
+            else
+            {
+                // 取消上一个选中物体的高亮
+                selectedObject.meshRenderer.material = selectedObject.defaultMaterial;
+            }
+        }
+
+        selectedObject = this;
         isClicked = true;
-        // Write code logic here to handle object click
+
+        if (ihaveChild)
+        {
+            foreach (ChildRenderers childRenderer in childRenderers)
+            {
+                childRenderer.childRenderer.material = childRenderer.childHighlightMaterial;
+            }
+        }
+        else
+        {
+            // 将当前物体的材质更改为高亮材质
+            meshRenderer.material = highlightMaterial;
+        }
 
         Debug.Log("Object Clicked");
 
-        ItemsReading.Instance.ReceiveClickedObject(gameObject); // Send clicked object information to ItemsReading
-        DisposeButtonScript.Instance.ReceiveClickedObject(gameObject); // Send clicked object information to DisposeButtonScript
-        ItemsManager.Instance._isCanRotate = true; // Change _isCanRotate to true
-
+        ItemsReading.Instance.ReceiveClickedObject(gameObject); // 将点击的物体信息发送给 ItemsReading
+        DisposeButtonScript.Instance.ReceiveClickedObject(gameObject); // 将点击的物体信息发送给 DisposeButtonScript
+        ItemsManager.Instance._isCanRotate = true; // 将 _isCanRotate 更改为 true
     }
 
     private void Update()
     {
         if (isClicked)
         {
-            // Logic after the object has been clicked
+            // 物体被点击后的逻辑处理
             isClicked = false;
         }
     }
 
     private void OnDestroy()
     {
+        if (selectedObject == this)
+        {
+            // 如果销毁的是选中的物体，则取消高亮
+            selectedObject = null;
+        }
+
         if (showList != null)
         {
             showList.OffList();
