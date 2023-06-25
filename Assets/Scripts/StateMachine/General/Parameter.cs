5ad41f370dca6bfabc31659d129d592449f7ed95
mixyao <chunshuaimiao@yahoo.com>
2023-06-26 07:57:22 +0800

    up_log

--- Assets/Scripts/StateMachine/General/Parameter.cs
+++ Assets/Scripts/StateMachine/General/Parameter.cs
@@ -9,28 +9,41 @@ namespace StateMachine.General
     public class Parameter
     {
         //It is a class that lists the parameter you need, I recommend you declare variables here!
-        public Transform[] patrolPoints;
-        public List<Transform> workPoints;
-        public Vector3 currentTarget;
+
+        
+        [Header("ifIdle")]
         public float idleTime;
         public float idleTimer;
+        
+        [Header("ifWork")]
+        public List<Transform> workPoints;
+        public Vector3 currentTarget;       
         public float workTimer;
-        public float patrolOuterRadius;
-        public float patrolInnerRadius;
-        public float moveSpeed;
-        public float hungrySpeed;
-        public bool isHungry;
-        public bool isTool;
-        public bool isWork;
         public GameObject BuildAnim;
         public GameObject Build_FinsihAnim;
         public GameObject Food_Tran;
+        
+        [Header("ifPatrol")]
+        public Transform[] patrolPoints;
+        public float patrolOuterRadius;
+        public float patrolInnerRadius;
+        public float moveSpeed;
+        
+        [Header("ifHungry")]
+        public float hungrySpeed;
+
+        [Header("ifDie")]
         public Texture2D did;
         public Texture2D flower;
-        [Header("if Clear")]
+        
+        [Header("if Clean")]
         public GameObject CleanWanter;
         public Transform[] cleanTargetList;
 
+        [Header("switch")]
+        public bool isHungry;
+        public bool isTool;
+        public bool isWork;
         public bool isClean;
         public bool isDie;
     }
