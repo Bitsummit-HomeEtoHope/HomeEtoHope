7eef38f6450ee6d4f009a2dfb984cf79752226f1
mixyao <chunshuaimiao@yahoo.com>
2023-06-26 15:15:41 +0800

    up_log

--- Assets/Scripts/Manager/DaysManager.cs
+++ Assets/Scripts/Manager/DaysManager.cs
@@ -1,16 +1,39 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class DaysManager : MonoBehaviour
 {
-    public enum Days { Day1, Day2, Day3 }
+    public enum Days { Day0,Day1, Day2, Day3 }
 
-    [SerializeField] private Days currentDay = Days.Day1;
-
+    [SerializeField] private Days currentDay = Days.Day0;
+    [Header("DayImage")]
     [SerializeField] private Image Days_Image; // 参考目标 UI 图像的 Image 组件
     [SerializeField] private Sprite Day2Image; // day1替换对象
     [SerializeField] private Sprite Day3Image; // day2替换对象
+    [SerializeField] public List<Sprite> humanlist;
 
+    [Header("List_Day1")]
+    [SerializeField] private Image List_Day1;
+    [SerializeField] public Image megane_1;
+    [SerializeField] public Image humanday_1;
+
+    [Header("List_Day2")]
+    [SerializeField] private Image List_Day2; 
+    [SerializeField] private Sprite day2_Day2; 
+    [SerializeField] public Image megane_2;
+    [SerializeField] public Image humanday_2;
+
+    [Header("List_Day3")]
+    [SerializeField] private Image List_Day3; 
+    [SerializeField] private Sprite day3_Day3; 
+    [SerializeField] public Image megane_3;
+    [SerializeField] public Image humanday_3;
+
+
+    private void Start()
+    {
+    }
 
     private void OnEnable()
     {
@@ -21,27 +44,71 @@ public class DaysManager : MonoBehaviour
     {
         switch (currentDay)
         {
+            case Days.Day0:
+                if ( megane_1 != null && humanlist.Count > 0)
+                {
+                    int randomIndex = Random.Range(0, humanlist.Count);
+                    Sprite randomSprite = humanlist[randomIndex];
+                    megane_1.sprite = randomSprite;
+                    humanday_1.transform.localScale = Vector3.one;
+
+                    //删掉
+                    humanlist.RemoveAt(randomIndex);
+                }
+                currentDay = Days.Day1;
+                break;
+
             case Days.Day1:
-                Debug.Log("Day1 functionality executed!");
                 // 更改目标 UI 图像的图片
-                Days_Image.sprite = Day2Image;
+                if (Days_Image != null)
+                {
+                    Days_Image.sprite = Day2Image;
+                }
+
+                List_Day2.sprite = day2_Day2;
+
+                if (megane_2 != null && humanlist.Count > 0)
+                {
+                    int randomIndex = Random.Range(0, humanlist.Count);
+                    Sprite randomSprite = humanlist[randomIndex];
+                    megane_2.sprite = randomSprite;
+                    humanday_2.transform.localScale = Vector3.one;
+
+                    humanlist.RemoveAt(randomIndex);
+                }
 
                 currentDay = Days.Day2;
                 break;
 
             case Days.Day2:
-                Debug.Log("Day2 functionality executed!");
+                if (Days_Image != null)
+                {
+                    Days_Image.sprite = Day3Image;
+                }
 
-                Days_Image.sprite = Day3Image;
+                // 将 humanday_1 的比例设置为 1
+                if (humanday_3 != null)
+                {
+                    humanday_3.transform.localScale = Vector3.one;
+                }
+
+                if (megane_3 != null && humanlist.Count > 0)
+                {
+                    int randomIndex = Random.Range(0, humanlist.Count);
+                    Sprite randomSprite = humanlist[randomIndex];
+                    megane_3.sprite = randomSprite;
+                    humanday_3.transform.localScale = Vector3.one;
+
+                    humanlist.RemoveAt(randomIndex);
+                }
+                List_Day3.sprite = day3_Day3;
 
                 currentDay = Days.Day3;
                 break;
 
-            case Days.Day3:
 
-
-                Debug.Log("Day3 functionality executed!");
-
+                case Days.Day3:
+                //TODO: 结束游戏转为结束场景。
                 break;
 
             default:
