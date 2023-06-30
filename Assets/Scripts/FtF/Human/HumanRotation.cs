63fcf0c6362b70a9bc60d60aa7eaec7dba251cca
mixyao <chunshuaimiao@yahoo.com>
2023-07-01 07:36:55 +0800

    up_human_rorate

--- Assets/Scripts/FtF/Human/HumanRotation.cs
+++ Assets/Scripts/FtF/Human/HumanRotation.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class HumanRotation : MonoBehaviour
 {
+    [SerializeField] public int rotation;
     private Transform characterTransform;
     private Vector3 previousPosition;
 
@@ -17,11 +18,11 @@ public class HumanRotation : MonoBehaviour
 
         if (currentPosition.x < previousPosition.x)
         {
-            characterTransform.rotation = Quaternion.Euler(0f, 0f, 0f); // 向左移动时，Y轴旋转为0度
+            characterTransform.rotation = Quaternion.Euler(rotation, 0f, 0f); // 向左移动时，Y轴旋转为0度
         }
         else if (currentPosition.x > previousPosition.x)
         {
-            characterTransform.rotation = Quaternion.Euler(0f, 180f, 0f); // 向右移动时，Y轴旋转为180度
+            characterTransform.rotation = Quaternion.Euler(-rotation, 180f, 0f); // 向右移动时，Y轴旋转为180度
         }
 
         previousPosition = currentPosition; // 更新前一位置
