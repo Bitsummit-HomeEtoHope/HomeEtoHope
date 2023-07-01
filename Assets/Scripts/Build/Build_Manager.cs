72f486f53193d9729f6a3108c9ce53bee9378fc5
mixyao <chunshuaimiao@yahoo.com>
2023-07-01 18:55:58 +0800

    up_log

--- Assets/Scripts/Build/Build_Manager.cs
+++ Assets/Scripts/Build/Build_Manager.cs
@@ -4,22 +4,25 @@ using UnityEngine;
 
 public class Build_Manager : MonoBehaviour
 {
+    [Header("Secend camera")]
     public Camera camera2d;
-    public List<GameObject> buildList = new List<GameObject>();
-    public bool isAllBuild = false;
-    public Transform targetPosition; // 目标位置游戏物体
-    public Transform targetRotation; // 目标旋转角度游戏物体
-    [SerializeField]public float fieldOfViewSpeed = 2f; // Field of View 变化速度
-    [SerializeField]public int targetFieldOfView = 45; // Field of View 变化速度
+    [SerializeField] public float fieldOfViewSpeed = 2f; // Field of View 变化速度
+    [SerializeField] public int targetFieldOfView = 45; // Field of View 目标值
     [SerializeField] public float cameraMoveSpeed = 2f; // 相机移动速度
+    [SerializeField]private float cameraMoveTimer = 0f; // 相机移动计时器
+    [SerializeField]public float cameraMoveDuration = 2f; // 相机移动总时间
+    [SerializeField]public Transform targetPosition; // 目标位置游戏物体
+    [SerializeField]public Transform targetRotation; // 目标旋转角度游戏物体
+    [Header("Change numbers")]
+    [SerializeField]public int requiredBuildCount = 4; // 需要的 Build_39 标签数量
+    public GameObject enableGameObject; // 启用的游戏物体
 
-    private float cameraMoveTimer = 0f; // 相机移动计时器
-    public float cameraMoveDuration = 2f; // 相机移动总时间
+
 
     private void Update()
     {
-        isAllBuild = IsAllBuild();
-        if (isAllBuild)
+        int buildCount = CountBuildsWithTag("Build_39");
+        if (buildCount >= requiredBuildCount)
         {
             // 平滑过渡相机的 Field of View 到目标值
             camera2d.fieldOfView = Mathf.Lerp(camera2d.fieldOfView, targetFieldOfView, Time.deltaTime * fieldOfViewSpeed);
@@ -33,10 +36,10 @@ public class Build_Manager : MonoBehaviour
             // 平滑过渡相机的 rotation 到目标旋转角度
             camera2d.transform.rotation = Quaternion.Lerp(camera2d.transform.rotation, targetRotation.rotation, Time.deltaTime * moveSpeed);
 
-            // 设置 buildList 中所有物体的 SetActive(true)
-            foreach (var item in buildList)
+            // 启用指定的游戏物体
+            if (enableGameObject != null)
             {
-                item.SetActive(true);
+                enableGameObject.SetActive(true);
             }
 
             // 更新相机移动计时器
@@ -44,17 +47,9 @@ public class Build_Manager : MonoBehaviour
         }
     }
 
-    public bool IsAllBuild()
+    private int CountBuildsWithTag(string tag)
     {
-        Transform[] allChildren = GetComponentsInChildren<Transform>();
-        foreach (Transform child in allChildren)
-        {
-            var childIsBuild = child.GetComponent<Check_HumanDistance>();
-            if (childIsBuild != null && !childIsBuild.isBuild)
-            {
-                return false;
-            }
-        }
-        return true;
+        GameObject[] buildObjects = GameObject.FindGameObjectsWithTag(tag);
+        return buildObjects.Length;
     }
 }
