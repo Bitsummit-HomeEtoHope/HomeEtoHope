38ee83eace838384922f3ecf6c7d0c64fc4d46ef
mixyao <chunshuaimiao@yahoo.com>
2023-07-02 14:30:47 +0800

    up_log//opsion_OK

--- Assets/Scripts/Manager/TimeManager.cs
+++ Assets/Scripts/Manager/TimeManager.cs
@@ -12,13 +12,15 @@ public class TimeManager : MonoBehaviour
     [SerializeField] private bool isPaused = false; // 控制暂停状态
     [SerializeField] public List<GameObject> targetObjects; // 多个游戏对象
     [Header("JoJo")]
-    [SerializeField] private string codeHolderScriptName = "CodeHolder";
-    [SerializeField] private string rotateScriptName = "Rotate";
-    [SerializeField] private List<Graphic> uiElementsToDisable;
-
+    public Button[] buttonsToDisable; // 指定需要禁用的按钮数组
+    private List<MeshCollider> meshColliders;
+    private List<BoxCollider> boxColliders;
+ 
+    public GameObject[] objectsToDisable; // 存储需要禁用的游戏物体数组
 
     private void OnEnable()
     {
+        GetCollider();
         Pause();
     }
 
@@ -30,51 +32,108 @@ public class TimeManager : MonoBehaviour
     public void Pause()
     {
         isPaused = true;
+
+        DisableButtons();
+
+        DisableObjects();
+
+        SetCollidersEnabled(false);
+
         Time.timeScale = 0f;
+        
+        
 
-        // 禁用指定UI元素的交互功能和可见性
-        foreach (Graphic uiElement in uiElementsToDisable)
-        {
-            uiElement.raycastTarget = false;
-            Color color = uiElement.color;
-            color.a = 0f;
-            uiElement.color = color;
-        }
-
-        // 禁用 CodeHolder 脚本
-        CodeHolder codeHolder = FindObjectOfType<CodeHolder>();
-        if (codeHolder != null)
-            codeHolder.enabled = false;
-
-        // 禁用 Rotate 脚本
-        Rotate rotate = FindObjectOfType<Rotate>();
-        if (rotate != null)
-            rotate.enabled = false;
     }
 
     public void Resume()
     {
         isPaused = false;
+
+        EnableButtons();
+
+        EnableObjects();
+
+        SetCollidersEnabled(true);
+
         Time.timeScale = 1f;
 
-        // 启用指定UI元素的交互功能和可见性
-        foreach (Graphic uiElement in uiElementsToDisable)
+        
+    }
+
+    private void GetCollider()
+    {
+        // 获取场景中所有的 Mesh Collider 组件
+        MeshCollider[] allMeshColliders = FindObjectsOfType<MeshCollider>();
+        meshColliders = new List<MeshCollider>();
+
+        foreach (MeshCollider collider in allMeshColliders)
         {
-            uiElement.raycastTarget = true;
-            Color color = uiElement.color;
-            color.a = 1f;
-            uiElement.color = color;
+            if (collider != null)
+            {
+                meshColliders.Add(collider);
+            }
         }
 
-        // 启用 CodeHolder 脚本
-        CodeHolder codeHolder = FindObjectOfType<CodeHolder>();
-        if (codeHolder != null)
-            codeHolder.enabled = true;
+        // 获取场景中所有的 Box Collider 组件
+        BoxCollider[] allBoxColliders = FindObjectsOfType<BoxCollider>();
+        boxColliders = new List<BoxCollider>();
 
-        // 启用 Rotate 脚本
-        Rotate rotate = FindObjectOfType<Rotate>();
-        if (rotate != null)
-            rotate.enabled = true;
+        foreach (BoxCollider collider in allBoxColliders)
+        {
+            if (collider != null)
+            {
+                boxColliders.Add(collider);
+            }
+        }
     }
+
+    private void SetCollidersEnabled(bool enabled)
+    {
+        // 设置 Mesh Collider 的启用状态
+        foreach (MeshCollider collider in meshColliders)
+        {
+            collider.enabled = enabled;
+        }
+
+        // 设置 Box Collider 的启用状态
+        foreach (BoxCollider collider in boxColliders)
+        {
+            collider.enabled = enabled;
+        }
+    }
+
+
+    public void DisableButtons()
+    {
+        foreach (Button button in buttonsToDisable)
+        {
+            button.interactable = false; // 禁用按钮的交互功能
+        }
+    }
+
+    public void EnableButtons()
+    {
+        foreach (Button button in buttonsToDisable)
+        {
+            button.interactable = true; // 启用按钮的交互功能
+        }
+    }
+
+    public void DisableObjects()
+    {
+        foreach (GameObject obj in objectsToDisable)
+        {
+            obj.SetActive(false); // 禁用游戏物体
+        }
+    }
+
+    public void EnableObjects()
+    {
+        foreach (GameObject obj in objectsToDisable)
+        {
+            obj.SetActive(true); // 启用游戏物体
+        }
+    }
+
 
 }
