d8c46b340d929856e8e3071327cf3be1a44956e9
mixyao <chunshuaimiao@yahoo.com>
2023-07-02 13:37:42 +0800

    up_log//maege_opsion//bug_still_last_one

--- Assets/Scripts/Manager/TimeManager.cs
+++ Assets/Scripts/Manager/TimeManager.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SocialPlatforms;
+using UnityEngine.UI;
 
 public class TimeManager : MonoBehaviour
 {
     [Header("Dio")]
     [SerializeField] private bool isPaused = false; // 控制暂停状态
     [SerializeField] public List<GameObject> targetObjects; // 多个游戏对象
+    [Header("JoJo")]
+    [SerializeField] private string codeHolderScriptName = "CodeHolder";
+    [SerializeField] private string rotateScriptName = "Rotate";
+    [SerializeField] private List<Graphic> uiElementsToDisable;
 
-    private List<GameObject> nonTargetObjects; // 非目标对象列表
 
     private void OnEnable()
     {
@@ -20,39 +27,54 @@ public class TimeManager : MonoBehaviour
         Resume();
     }
 
-    private void Start()
-    {
-        nonTargetObjects = new List<GameObject>();
-        GameObject[] allObjects = GameObject.FindObjectsOfType<GameObject>();
-        foreach (GameObject obj in allObjects)
-        {
-            if (!targetObjects.Contains(obj))
-            {
-                nonTargetObjects.Add(obj);
-            }
-        }
-    }
-
-    private void Update()
-    {
-        if (isPaused)
-        {
-            SetTimeScale(0f);
-        }
-    }
-
-    private void SetTimeScale(float timeScale)
-    {
-        Time.timeScale = timeScale;
-    }
-
     public void Pause()
     {
         isPaused = true;
+        Time.timeScale = 0f;
+
+        // 禁用指定UI元素的交互功能和可见性
+        foreach (Graphic uiElement in uiElementsToDisable)
+        {
+            uiElement.raycastTarget = false;
+            Color color = uiElement.color;
+            color.a = 0f;
+            uiElement.color = color;
+        }
+
+        // 禁用 CodeHolder 脚本
+        CodeHolder codeHolder = FindObjectOfType<CodeHolder>();
+        if (codeHolder != null)
+            codeHolder.enabled = false;
+
+        // 禁用 Rotate 脚本
+        Rotate rotate = FindObjectOfType<Rotate>();
+        if (rotate != null)
+            rotate.enabled = false;
     }
 
     public void Resume()
     {
-        SetTimeScale(1f);        
+        isPaused = false;
+        Time.timeScale = 1f;
+
+        // 启用指定UI元素的交互功能和可见性
+        foreach (Graphic uiElement in uiElementsToDisable)
+        {
+            uiElement.raycastTarget = true;
+            Color color = uiElement.color;
+            color.a = 1f;
+            uiElement.color = color;
+        }
+
+        // 启用 CodeHolder 脚本
+        CodeHolder codeHolder = FindObjectOfType<CodeHolder>();
+        if (codeHolder != null)
+            codeHolder.enabled = true;
+
+        // 启用 Rotate 脚本
+        Rotate rotate = FindObjectOfType<Rotate>();
+        if (rotate != null)
+            rotate.enabled = true;
     }
+
 }
