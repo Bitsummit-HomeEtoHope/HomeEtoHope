dd6c56830e57732b55b71a9994f9a34b2c82dd0d
mixyao <chunshuaimiao@yahoo.com>
2023-07-14 12:39:41 +0800

    up_log/title_button

--- Assets/Scripts/Title/ChangeScene.cs
+++ Assets/Scripts/Title/ChangeScene.cs
@@ -14,8 +14,12 @@ public class ChangeScene : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     public Button hardButton;
     public Image redImage; // 指定的红色Image
 
+    private float pressDuration = 3f; // 长按的持续时间
     private bool isPressing = false; // 记录按钮是否处于按下状态
     private float pressStartTime; // 记录按下按钮的起始时间
+
+    private bool isPressed = false; // 按钮是否被按下
+    private float timer = 0f; // 计时器
 
     private void Start()
     {
@@ -24,13 +28,11 @@ public class ChangeScene : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 
     private void Update()
     {
-        if (isPressing)
+        if (isPressed)
         {
-            float elapsedTime = Time.time - pressStartTime;
-            float fillAmount = Mathf.Clamp01(elapsedTime / 3f);
-            redImage.fillAmount = fillAmount;
+            timer += Time.deltaTime;
 
-            if (elapsedTime >= 3f)
+            if (timer >= 3f)
             {
                 SceneManager.LoadScene("Level2");
             }
@@ -39,22 +41,53 @@ public class ChangeScene : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 
     private void OnButtonPressed()
     {
-        isPressing = true;
-        pressStartTime = Time.time;
+        isPressed = true;
+        timer = 0f;
     }
 
     private void OnButtonReleased()
     {
-        isPressing = false;
-        redImage.fillAmount = 0f;
+        isPressed = false;
+        timer = 0f;
+    }
+
+    private void Awake()
+    {
+        if (playButton != null && exitButton != null && howButton != null && listButton != null && hardButton != null)
+            IN();
+    }
+
+    public void PlayGame()
+    {
+        if (!isPressing)
+        {
+            // 执行单击切换场景的逻辑
+            SceneManager.LoadScene("Level1");
+        }
+    }
+
+    public void ExitGame()
+    {
+        if (playButton != null && exitButton != null && howButton != null && listButton != null && hardButton != null)
+            OUT();
+
+        EndGame();
+    }
+
+    public void HowToPlay()
+    {
+        if (playButton != null && exitButton != null && howButton != null && listButton != null && hardButton != null)
+            OUT();
+
+        SceneManager.LoadScene("TellYou");
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
         if (eventData.pointerEnter == hardButton.gameObject)
         {
-            // 开始长按计时
-            StartCoroutine(LongPressTimer());
+            // 开始长按计时的协程
+            StartCoroutine(StartLongPressTimer());
         }
     }
 
@@ -65,38 +98,50 @@ public class ChangeScene : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
             // 松开按钮后重置红色Image的FillAmount为0
             redImage.fillAmount = 0f;
 
+            // 重置长按状态
+            isPressing = false;
+
             // 停止计时器协程
-            StopCoroutine(LongPressTimer());
+            StopCoroutine(StartLongPressTimer());
         }
     }
 
-    private IEnumerator LongPressTimer()
+    private IEnumerator StartLongPressTimer()
     {
-        yield return new WaitForSeconds(3f);
+        float elapsedTime = 0f; // 记录经过的时间
 
-        if (isPressing)
+        // 记录按下按钮的起始时间
+        pressStartTime = Time.time;
+        isPressing = true;
+
+        while (isPressing)
         {
-            SceneManager.LoadScene("Level2");
+            elapsedTime = Time.time - pressStartTime;
+
+            // 更新红色Image的FillAmount
+            float fillAmount = Mathf.Clamp01(elapsedTime / pressDuration);
+            redImage.fillAmount = fillAmount;
+
+            // 如果长按的持续时间超过预设的时间，执行长按切换场景的逻辑
+            if (elapsedTime >= pressDuration)
+            {
+                // 执行长按切换场景的逻辑
+                SceneManager.LoadScene("Level2");
+                break;
+            }
+
+            yield return null;
         }
-    }
 
-    public void PlayGame()
-    {
-        SceneManager.LoadScene("Level1");
-    }
-
-    public void ExitGame()
-    {
-        EndGame();
-    }
-
-    public void HowToPlay()
-    {
-        SceneManager.LoadScene("TellYou");
+        // 松开按钮后重置红色Image的FillAmount为0
+        redImage.fillAmount = 0f;
     }
 
     public void OurList()
     {
+        if (playButton != null && exitButton != null && howButton != null && listButton != null && hardButton != null)
+            OUT();
+
         SceneManager.LoadScene("OurList");
     }
 
@@ -105,8 +150,13 @@ public class ChangeScene : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #else
-        Application.Quit();
+        QuitGame();
 #endif
+    }
+
+    private void QuitGame()
+    {
+        Application.Quit();
     }
 
     private void IN()
@@ -123,5 +173,8 @@ public class ChangeScene : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
         exitButton.onClick.RemoveListener(ExitGame);
         howButton.onClick.RemoveListener(HowToPlay);
         listButton.onClick.RemoveListener(OurList);
+
+        // 重置长按状态
+        isPressing = false;
     }
 }
