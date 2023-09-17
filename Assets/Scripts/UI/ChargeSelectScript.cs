55611d5d6e87799cf2b6be4a5be4aff54bb94d95
mixyao <chunshuaimiao@yahoo.com>
2023-09-17 12:08:50 +0800

    TODO:組合わせ

---
+++ Assets/Scripts/UI/ChargeSelectScript.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChargeSelectScript : MonoBehaviour
+{
+    private string _SelectKanban;
+    private Image _iconImage;
+    private Image _buttonImage;
+    private Transform _buttonTransform;
+    private Transform _iconTransform;
+
+    private bool _isSelected = false;
+    
+    [SerializeField] private GameObject thekanban;
+    [SerializeField] private GameObject theEnerugi;
+    private SpriteRenderer _kanbanIcon;
+    
+
+    private void Start()
+    {
+        SelectEnerugi("No");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            EnerugiCharge();
+        }
+    }
+
+    public void SelectEnerugi(string toKanban)
+    {
+        _SelectKanban = toKanban;
+        SelectChange();
+        IconChange();
+    }
+
+    private void SelectChange()
+    {
+        switch (_SelectKanban)
+        {
+            case "No":
+                break;
+            case "Tool":
+                IconReset();
+                _isSelected = true;
+                theEnerugi = GameObject.Find("enerugi_tool");
+                thekanban = GameObject.Find("kanban_tool");
+                _buttonTransform = GetComponent<Transform>().Find("ToolSelectButton");
+                break;
+            case "Human":
+                IconReset();
+                _isSelected = true;
+                theEnerugi = GameObject.Find("enerugi_human");
+                thekanban = GameObject.Find("kanban_human");
+                _buttonTransform = GetComponent<Transform>().Find("HumanSelectButton");
+                break;
+            case "Food":
+                IconReset();
+                _isSelected = true;
+                theEnerugi = GameObject.Find("enerugi_food");
+                thekanban = GameObject.Find("kanban_food");
+                _buttonTransform = GetComponent<Transform>().Find("FoodSelectButton");
+                break;
+        }
+        _iconTransform = _buttonTransform.GetComponent<Transform>().Find("Image");
+
+        _kanbanIcon = thekanban.GetComponent<SpriteRenderer>();
+        _buttonImage = _buttonTransform.GetComponent<Image>();
+        _iconImage = _iconTransform.GetComponent<Image>();
+
+    }
+
+    private void IconChange()
+    {
+        _kanbanIcon.color = Color.green;
+        _buttonImage.color = Color.green;
+        _iconImage.color = new Color(0.5f, 0, 0); // 0, 0.5, 0
+    }
+
+    private void IconReset()
+    {
+        if (_isSelected)
+        {
+            _kanbanIcon.color = Color.white;
+            _buttonImage.color = new Color(0, 0.5f, 0);
+            _iconImage.color = Color.white;
+        }
+    }
+
+    private void EnerugiCharge()
+    {
+        EnerugiScript enerugiScript = theEnerugi.GetComponent<EnerugiScript>();
+        enerugiScript.PowerUp();
+    }
+}
--- Assets/Scripts/UI/enerugiScript.cs
+++ Assets/Scripts/UI/enerugiScript.cs
@@ -1,46 +1,129 @@
+using Newtonsoft.Json.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class enerugiScript : MonoBehaviour
+public class EnerugiScript : MonoBehaviour
 {
     public List<GameObject> enerugiList = new List<GameObject>();
 
+    public float powerChangeTimeTool = 5;
+    public float powerChangeTimeHuman = 7;
+    public float powerChangeTimeFood = 5;
 
-    public float powerChangeTime = 3;
-   
+    public bool powerIsZero = false;
+
+    private Animator _animator;
+    private GameObject _power;
+    private int _powerCunt = 5;
+    private bool _isTimerRunning;
+    private float _powerChangeTime;
+    
     // Start is called before the first frame update
+
     void Start()
     {
-        
+        //transform powertransform = transform.find("power");
+        switch (this.name)
+        {
+            case "enerugi_tool":
+                _powerChangeTime = powerChangeTimeTool;
+                break;
+            case "enerugi_human":
+                _powerChangeTime = powerChangeTimeHuman;
+                break;
+            case "enerugi_food":
+                _powerChangeTime = powerChangeTimeFood;
+                break;
+        }
+
+        SetPower();
+        StartTimer();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.I))
+        if (_powerCunt == 0)
         {
-            powerUp();
-        }     
+
+        }
+        else
+        {
+
+        }
     }
 
-    public void powerUp()
+    private void SetPower()
+    {
+        _power = enerugiList[_powerCunt - 1];
+        _animator = _power.GetComponent<Animator>();
+    }
+
+    public void PowerUp()
+    {
+        if (_powerCunt == 5) return;
+
+        _powerCunt++;
+        SetPower();
+        _animator.SetTrigger("powerUp");
+        PowerChange();
+    }
+
+    public void PowerDown()
+    {
+        SetPower();
+        _powerCunt--;
+        _animator.SetTrigger("powerDown");
+        PowerChange();
+    }
+
+    public void PowerChange()
+    {
+        foreach (GameObject obj in enerugiList)
+        {
+            Animator animator = obj.GetComponent<Animator>();
+            if (animator != null)
+            {
+                animator.SetInteger("powerCunt", _powerCunt);
+                animator.SetTrigger("powerChange");
+            }
+        }
+    }
+
+    public void PowerZero()
     {
 
     }
 
-    public void powerDown()
+    private void StartTimer()
     {
-
+        if (!_isTimerRunning)
+        {
+            StartCoroutine(RunTimer());
+        }
     }
 
-    public void powerChange()
+    private void ResetTimer()
     {
-
+        StopAllCoroutines();
+        StartTimer();
     }
 
-    public void powerZero()
+    private IEnumerator RunTimer()
     {
+        _isTimerRunning = true;
+        float currentTime = 0;
 
+        while (currentTime < _powerChangeTime)
+        {
+            currentTime += Time.deltaTime;
+            yield return null; // µÈ´ýÒ»Ö¡
+        }
+
+        if (0 < _powerCunt) PowerDown();
+
+        _isTimerRunning = false;
+        StartTimer();
     }
 }
