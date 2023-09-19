bbaa0e589fb9e45ee96684202a20e9acea29577d
mixyao <chunshuaimiao@yahoo.com>
2023-09-20 01:28:37 +0800

    bettery

--- Assets/opsion/opsionopen.cs
+++ Assets/opsion/opsionopen.cs
@@ -6,7 +6,7 @@ using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-public class OpsionOpen : MonoBehaviour
+public class opsionOpen : MonoBehaviour
 {
     //ê›íËópÇÃcanvas
     public GameObject Option_Canvas;
