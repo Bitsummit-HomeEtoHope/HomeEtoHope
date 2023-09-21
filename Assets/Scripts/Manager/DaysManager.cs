c255385f82e87d569af009b9691117cf6dfde19e
mixyao <chunshuaimiao@yahoo.com>
2023-09-21 13:00:31 +0900

    ending

--- Assets/Scripts/Manager/DaysManager.cs
+++ Assets/Scripts/Manager/DaysManager.cs
@@ -109,15 +109,18 @@ public class DaysManager : MonoBehaviour
             obj.SetActive(false);
 
         }
-        Destroy(postTxt);
+       // Destroy(postTxt);
 
     }
 
     private void CloseObjectsInList(List<GameObject> objectList)
     {
-        foreach (var obj in objectList)
+        if (objectList != null || objectList.Count != 0)
         {
-            obj.SetActive(false);
+            foreach (var obj in objectList)
+            {
+                if(!obj.activeSelf)obj.SetActive(false);
+            }
         }
     }
     public void daysChange()
